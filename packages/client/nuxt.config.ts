1b79359f80ff1e0d60a554bb0bb9e163c47c92de
bq-t <w.bequiet@gmail.com>
2025-05-07 08:18:57 +0400

    feat(client): init application css

--- packages/client/nuxt.config.ts
+++ packages/client/nuxt.config.ts
@@ -1,10 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  css: ['~/assets/css/main.css'],
   modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui', '@nuxtjs/strapi'],
   devtools: { enabled: true },
   typescript: { typeCheck: true },
-  ui: { colorMode: false },
+  ui: {
+    colorMode: false,
+    theme: {
+      colors: ['black', 'white'],
+    },
+  },
   image: {
     provider: 'cloudinary',
     cloudinary: {
