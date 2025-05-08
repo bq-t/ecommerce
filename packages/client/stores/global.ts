5e46694bea80896feb28c960642bae83f2bc90e5
bq-t <w.bequiet@gmail.com>
2025-05-08 09:22:30 +0400

    feat(client): add global model fetching on website load

--- packages/client/stores/global.ts
+++ packages/client/stores/global.ts
@@ -3,6 +3,52 @@ import type { Global } from '@/types/models/single/global'
 export const useGlobalStore = defineStore('globalStore', () => {
   const state = useState<Global>('global')
 
+  const fetchConfig = async () => {
+    const { findOne } = useStrapi()
+
+    try {
+      await callOnce('global', async () => {
+        const { data } = await findOne<Global>('global', {
+          populate: {
+            header: {
+              populate: {
+                logotype: {
+                  fields: ['url', 'name', 'alternativeText'],
+                },
+              },
+            },
+            navbar: {
+              populate: ['items'],
+            },
+            footer: {
+              populate: {
+                logotype: {
+                  fields: ['url', 'name', 'alternativeText'],
+                },
+                linkGroups: {
+                  populate: ['links'],
+                },
+                socials: true,
+              },
+            },
+            settings: {
+              populate: ['features'],
+            },
+          },
+        })
+        state.value = data
+      })
+      /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
+    } catch (e) {
+      throw createError({
+        statusCode: 503,
+        statusMessage: 'Failed to fetch global model',
+        message:
+          'Please initialize the Global model before running the application',
+      })
+    }
+  }
+
   const settings = computed(() => state.value.settings)
 
   const headerData = computed(() => state.value.header)
@@ -14,5 +60,6 @@ export const useGlobalStore = defineStore('globalStore', () => {
     headerData,
     navbarData,
     footerData,
+    fetchConfig,
   }
 })
